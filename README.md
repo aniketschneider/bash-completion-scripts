2278b2315bf7e0b1d41abdd29051ebbee7edccee
Aniket Schneider <aschneider@visiblemeasures.com>
2012-11-15 21:47:17 -0500

    Capistrano task completion

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ bash-completion
 ===============
 
 Some custom bash-completion scripts
+
+installation
+============
+
+Make sure you have bash completion installed
+
+Source the completion script in your .bashrc or .bash\_profile
---
+++ cap.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# bash completion for capistrano tasks
+#
+# Generates a cache file (${HOME}/.cap_completion_cache) to avoid having to run cap -T for every 
+# consecutive completion attempt. The cache file is renewed whenever completion is attempted from
+# a different path, or after a timeout.
+
+_cap_complete ()
+{
+  local cache_file="${HOME}/.cap_completion_cache"
+  local cur=${COMP_WORDS[COMP_CWORD]}
+
+  COMPREPLY=()
+
+  # try to expand anything but options (starting with -)
+  if [[ $cur != -* ]] ; then
+    # refresh cache if it doesn't exist, is for the wrong path, or is too old
+    if [[ ! -e $cache_file ]] || 
+       [[ $(pwd) != $(head -n1 $cache_file) ]] ||
+       [ `find "$cache_file" -mmin +30` ] ; then
+      echo "$(pwd)" >$cache_file
+      cap -qT 2>/dev/null | ruby -ne 'puts $_[/^cap (\S+) .*$/,1] if $_ =~ /^cap/' >>$cache_file
+    fi
+    COMPREPLY=( $(compgen -W "$(tail +2 $cache_file)" -- $cur) )
+  fi
+
+} 
+complete -F _cap_complete -o default cap
+
+# allow completion of commands including colons
+COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
